cdad3c1cda8afc9a051a1da3a0b5657b38b5d661
Graham Naquin <graham@naquin.live>
2021-02-20 11:29:26 -0600

    fixed IP in deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.86.216.163',
+      host: '54.236.46.215',
       username: 'ubuntu',
       privateKey: 'ho1-key.pem'
     })

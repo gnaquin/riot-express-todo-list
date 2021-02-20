69eff63b77cd2a0691e9b5babf5747fc8e65a320
Graham Naquin <70065688+gnaquin@users.noreply.github.com>
2021-02-20 12:38:03 -0600

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.236.46.215',
+      host: '34.232.68.101',
       username: 'ubuntu',
       privateKey: 'ho1-key.pem'
     })
